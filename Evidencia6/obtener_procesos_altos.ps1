7e771db545052b6079c588f81cd3534be7deef47
EdRmzT <edgar.ramirezt@uanl.edu.mx>
2025-09-23 10:04:16 -0600

    Update obtener_procesos_altos.ps1

--- Evidencia6/obtener_procesos_altos.ps1
+++ Evidencia6/obtener_procesos_altos.ps1
@@ -1,3 +1,6 @@
+Editado por <<EdgarRmz>> el <<23/09/2025>>
+
 $DDMMYYYY= Get-Date -Format "dd_MM_yyyy"
 $fILENAME= "Procesos Filtrados ($DDMMYYYY).txt"
+
 Get-Process|Where-object -Property "CPU" -gt "1.0"|sort-object|Out-File $fILENAME -Encoding utf8
